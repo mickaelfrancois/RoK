5ba898912c1d849d5ee3d43ceaa4a661b226a9aa
Mickaël FRANCOIS <mickael.francois@fpc-france.com>
2025-10-13 14:54:17 +0200

    refactor(dto): make DTOs independent from entities

--- Core/Rok.Application/Dto/AlbumDto.cs
+++ Core/Rok.Application/Dto/AlbumDto.cs
@@ -1,5 +1,74 @@
-﻿namespace Rok.Application.Dto;
+﻿using Dapper.Contrib.Extensions;
 
-public class AlbumDto : AlbumEntity
+namespace Rok.Application.Dto;
+
+public class AlbumDto
 {
+    public override string ToString() => Name;
+
+    public long Id { get; set; }
+
+    public DateTime CreatDate { get; set; }
+
+    public DateTime? EditDate { get; set; }
+    
+    public string Name { get; set; } = string.Empty;
+
+    public int? Year { get; set; }
+
+    public bool IsLive { get; set; } = false;
+
+    public bool IsCompilation { get; set; } = false;
+
+    public bool IsBestOf { get; set; } = false;
+
+    public string? Wikipedia { get; set; }
+
+    public string? NovaUid { get; set; }
+
+    public int TrackCount { get; set; } = 0;
+
+    public long Duration { get; set; } = 0;
+
+    public DateTime? ReleaseDate { get; set; }
+
+    public string? Label { get; set; }
+
+    public string? Speed { get; set; }
+
+    public string? Theme { get; set; }
+
+    public string? Mood { get; set; }
+
+    public string? Sales { get; set; }
+
+    public string? ReleaseFormat { get; set; }
+
+    public string? MusicBrainzID { get; set; }
+
+    public string AlbumPath { get; set; } = string.Empty;
+
+    public long? ArtistId { get; set; }
+
+    public long? GenreId { get; set; }
+
+    public bool IsFavorite { get; set; }
+
+    public int ListenCount { get; set; }
+
+    public DateTime? LastListen { get; set; }
+
+    public DateTime? GetMetaDataLastAttempt { get; set; }
+
+    public string GenreName { get; set; } = string.Empty;
+
+    public bool IsGenreFavorite { get; set; }
+
+    public string ArtistName { get; set; } = string.Empty;
+
+    public bool IsArtistFavorite { get; set; }
+
+    public string CountryCode { get; set; } = string.Empty;
+
+    public string CountryName { get; set; } = string.Empty;
 }
--- Core/Rok.Application/Dto/ArtistDto.cs
+++ Core/Rok.Application/Dto/ArtistDto.cs
@@ -1,5 +1,84 @@
-﻿namespace Rok.Application.Dto;
+﻿using Dapper.Contrib.Extensions;
 
-public class ArtistDto : ArtistEntity
+namespace Rok.Application.Dto;
+
+public class ArtistDto
 {
+    public override string ToString() => Name;
+
+    public long Id { get; set; }
+
+    public DateTime CreatDate { get; set; }
+
+    public DateTime? EditDate { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? WikipediaUrl { get; set; }
+
+    public string? OfficialSiteUrl { get; set; }
+
+    public string? FacebookUrl { get; set; }
+
+    public string? TwitterUrl { get; set; }
+
+    public string? NovaUid { get; set; }
+
+    public string? MusicBrainzID { get; set; }
+
+    public int? YearMini { get; set; }
+
+    public int? YearMaxi { get; set; }
+
+    public int TrackCount { get; set; } = 0;
+
+    public int AlbumCount { get; set; } = 0;
+
+    public int LiveCount { get; set; } = 0;
+
+    public int CompilationCount { get; set; }
+
+    public int BestofCount { get; set; } = 0;
+
+    public bool IsFavorite { get; set; }
+
+    public int ListenCount { get; set; }
+
+    public DateTime? LastListen { get; set; }
+
+    public long? GenreId { get; set; }
+
+    public long? CountryId { get; set; }
+
+    public long TotalDurationSeconds { get; set; } = 0;
+
+    public int? FormedYear { get; set; }
+
+    public int? BornYear { get; set; }
+
+    public int? DiedYear { get; set; }
+
+    public bool Disbanded { get; set; }
+
+    public string? Style { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Mood { get; set; }
+
+    public string? Members { get; set; }
+
+    public string? SimilarArtists { get; set; }
+
+    public string? Biography { get; set; }
+
+    public DateTime? GetMetaDataLastAttempt { get; set; }
+
+    public string GenreName { get; set; } = string.Empty;
+
+    public bool IsGenreFavorite { get; set; }
+
+    public string CountryCode { get; set; } = string.Empty;
+
+    public string CountryName { get; set; } = string.Empty;
 }
