af7a300e6d1e3e4bdd12edf781e69dbf23de0e7b
Mickaël FRANCOIS <mickael@francois.ovh>
2025-11-19 21:40:19 +0100

    feat: add file helper to check if a file is online

---
+++ Core/Rok.Import/FileHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace Rok.Import;
+
+static internal class FileHelpers
+{
+    private const int FILE_ATTRIBUTE_RECALL_ON_OPEN = 0x00400000;
+
+    public static bool IsOnline(string file)
+    {
+        FileAttributes attributes = File.GetAttributes(file);
+
+        bool isOnline = (((int)attributes) & FILE_ATTRIBUTE_RECALL_ON_OPEN) != 0;
+
+        return isOnline;
+    }
+}
