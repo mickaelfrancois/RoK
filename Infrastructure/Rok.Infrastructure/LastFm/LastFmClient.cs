5b5c7771ffd42844dc2c6acc8ab53c9f8eddaef5
Mickaël FRANCOIS <mickael@francois.ovh>
2025-12-03 13:55:43 +0100

    fix: user standard user agent for lastfm

--- Infrastructure/Rok.Infrastructure/LastFm/LastFmClient.cs
+++ Infrastructure/Rok.Infrastructure/LastFm/LastFmClient.cs
@@ -88,7 +88,8 @@ internal class LastFmClient : ILastFmClient
         string appVersion = GetAppVersion();
 
         _httpClient.BaseAddress = new Uri(UriString);
-        _httpClient.DefaultRequestHeaders.Add("User-Agent", $"Rok/{appVersion}");
+        //        _httpClient.DefaultRequestHeaders.Add("User-Agent", $"Rok/{appVersion}");
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
         _httpClient.Timeout = TimeSpan.FromSeconds(5);
     }
 
