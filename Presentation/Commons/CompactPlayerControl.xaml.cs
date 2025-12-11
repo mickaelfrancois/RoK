171d7ef1ed663651aac080bd3c89cf79d0f13435
Mickaël FRANCOIS <mickael@francois.ovh>
2025-12-11 23:07:05 +0100

    Improve CompactPlayer

--- Presentation/Commons/CompactPlayerControl.xaml.cs
+++ Presentation/Commons/CompactPlayerControl.xaml.cs
@@ -1,9 +1,6 @@
 using Microsoft.UI.Xaml.Controls;
 using Rok.Logic.ViewModels.Player;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Rok.Commons;
 
 public sealed partial class CompactPlayerControl : UserControl
