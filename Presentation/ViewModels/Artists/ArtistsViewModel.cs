25c15374b165748d8344bdf8e03ab0990b28e217
Mickaël FRANCOIS <mickael@francois.ovh>
2026-02-27 15:34:29 +0100

    Add artists stats

--- Presentation/ViewModels/Artists/ArtistsViewModel.cs
+++ Presentation/ViewModels/Artists/ArtistsViewModel.cs
@@ -34,6 +34,9 @@ public partial class ArtistsViewModel : ObservableObject, IDisposable
     public IReadOnlyList<string> SelectedTagFilters => _stateManager.SelectedTagFilters;
     public bool IsSelectedItems => _selectionManager.IsSelectedItems;
 
+    public int Count => _filteredArtists.Count;
+    public string DurationText => TimeSpan.FromSeconds(_filteredArtists.Sum(artist => artist.Artist.TotalDurationSeconds)).TotalHours.ToString("#0");
+
     [ObservableProperty]
     public partial string FilterByText { get; set; } = string.Empty;
 
