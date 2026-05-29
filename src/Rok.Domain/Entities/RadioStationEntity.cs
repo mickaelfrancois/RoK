7e694d61085ad34c480df08fdae35270cf30d0db
Mickaël FRANCOIS <mickael@francois.ovh>
2026-05-29 11:40:58 +0200

    feat(radio): add StationUuid/FaviconUrl/CountryCode/Codec/Bitrate to RadioStationEntity

--- src/Rok.Domain/Entities/RadioStationEntity.cs
+++ src/Rok.Domain/Entities/RadioStationEntity.cs
@@ -9,6 +9,16 @@ public class RadioStationEntity : BaseEntity
 
     public string? HomepageUrl { get; set; }
 
+    public string? StationUuid { get; set; }
+
+    public string? FaviconUrl { get; set; }
+
+    public string? CountryCode { get; set; }
+
+    public string? Codec { get; set; }
+
+    public int? Bitrate { get; set; }
+
     public DateTime AddedAt { get; set; }
 
     public DateTime? LastListen { get; set; }
